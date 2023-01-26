6cfe3d2e9ab2251b1cabbaefec5946527eb62f61
Moksleivis <Moksleivis@kitm.local>
2023-01-26 09:19:37 +0200

    second commit

---
+++ modules/firebase.js
@@ -0,0 +1,11 @@
+ // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyAGL6AlstPjBLBvw7mxioJLyPzdc_lz0Ro",
+    authDomain: "login-uzduotis.firebaseapp.com",
+    projectId: "login-uzduotis",
+    storageBucket: "login-uzduotis.appspot.com",
+    messagingSenderId: "684908319806",
+    appId: "1:684908319806:web:60ad99fd2c93e0387c6d6f"
+  };
+
+  export {firebaseConfig}
---
+++ modules/main.js
@@ -0,0 +1,2 @@
+import { initializeApp } 
+from "https://www.gstatic.com/firebasejs/9.16.0/firebase-app.js";
